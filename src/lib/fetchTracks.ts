5140d6a7c9ea595e03f52974c79b97b7b20e27cf
LeVVej <135155373+LeVVej@users.noreply.github.com>
2023-05-31 21:18:43 +0200

    1 file modified

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,6 +1,6 @@
 import { SavedTrack, SpotifyType } from "spotify-types";
 
-const apiToken: string = 'BQCPxWndiLXGBZZ_TypFX05X01edLYTFF0z_HUIbBKu64miA3-ZM0KJ6rdES47hY1hTNMsOHV2-Z0mrFXSKhvuU6u9EszKPLsSV4VjfeBFLEzomXe1RztaFTJiildNZVH8x_P5MXhSNotujFTflj6EzGFlttymACyJ9wq1Wx6KbhzHojxff2x06m8FToNNQJim1yT3E4iTEo8OZXuvun2g';
+const apiToken: string = 'BBQDtFpPDYTw4fhT2QfqJr1JE51iBtbojBD0FnAn3O8kTAYG8vLRmdQg6SO1imhGDPs3yXahVgJgj6cpZUB1mhYAFgPuG1p8Wzp346xKhVxp5ouj5t8pvHGUixrXe9LypXsDu8k-5pjnDyw5L6NZW9CMVHDvofM0jEZP0ASzhW0YghmCRAG1-c1wZRpk60oXIAWuvIuBkShgns4QI7NdfrA';
 
 export const fetchTracks = async () :Promise<SavedTrack> => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
