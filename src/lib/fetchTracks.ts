e8a57f51cc7b731af8b0e9a64d6bc220c01f8dac
LeVVej <135155373+LeVVej@users.noreply.github.com>
2023-05-31 20:05:10 +0200

    1 file modified

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,4 +1,4 @@
-const apiToken: string = '';
+const apiToken: string = 'BQBlMZnJwxGEWR_nXLG1Gp4k_8Jud583ZnjZeR9aedjOnxMvAtd1948nZMNgRFiKtZmtcZuXqSqkiylUQ_0zE9fPpf-dDZBiLJAtIgex0iqrEUiTzZgoeIk3S2aVq-W3ZQyGaxJ6Z-qY1nTeiP_vqokZaaYqJGHl6dpvBk6W0GGqsZA0gk9d1sef00SfakIbqbRoVztpdO5rSt-ezGozb6caTLk';
 
 export const fetchTracks = async () => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
